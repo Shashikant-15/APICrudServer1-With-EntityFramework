165d74567cbce94b6f5fcc60ec3d9d23662d03fb
Shashikant-15 <shashikant.tanti@nashtechglobal.com>
2023-10-25 22:50:17 +0530

    Add project files.

---
+++ APICrudServer1/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICrudServer1
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(75)]
+        public string FirstName { get; set; } = "";
+
+        [StringLength(75)]
+        public string LastName { get; set; } = "";
+
+        [StringLength(50)]
+        public string PhoneNo { get; set; } = "";
+
+        [StringLength(75)]
+        public string EmailId { get; set; } = "";
+
+    }
+}
---
+++ APICrudServer1/Data/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APICrudServer1.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+
+    }
+}
